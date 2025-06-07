9ce9fe31fb6ffafd926b76dab00525f1f044dd39
o.o <96mnk28@gmail.com>
2025-06-07 14:20:21 -0400

    0.0.4
    added new tags and changed itemidtable values

--- PriceCheck.lua
+++ PriceCheck.lua
@@ -1,21 +1,27 @@
 addon.name      = 'PriceCheck';
 addon.author    = 'Ethoc';
-addon.version   = '0.0.3';
+addon.version   = '0.0.4';
 addon.desc      = 'Display the Base npc price of the item being looked at';
 addon.link      = 'https://github.com/96mnk28/PriceCheck';
 
--- TODO save textbox position when exiting
-
+--[[ TODO 
+    save textbox position when exiting
+    currently dosnt work with gamepad because i run on key input. . . Change event register to something else
+    add description tag to some items that are used in hxi quests
+    add the ability to toggle lines of the text on and off 
+--]]
+--Horizon Addon approval 
+--see ticket general-contact-1008
 
 require('common');
 local ItemIDTable = require('ItemIDTable');
 local fonts = require('fonts');
 local PriceCheck = T{};
-
+local useHxiData = 1-- set this to 1 to use hxi data set to 0 to use just lsb data
 local fontSettings = T{
 	visible = true,
 	font_family = 'Calibri',--Arial
-	font_height = 12,
+	font_height = 13,
 	color = 0xFFFFFFFF,
 	position_x = 400,
 	position_y = 900,
@@ -30,28 +36,7 @@ PriceCheck.Initialize = function()
 end
 PriceCheck.Initialize();
 
--- debugging text box
-
-local Debug = T{};
-local fontSettings2 = T{
-	visible = true,
-	font_family = 'Arial',
-	font_height = 12,
-	color = 0xFFFFFFFF,
-	position_x = 400,
-	position_y = 700,
-	background = T{
-		visible = true,
-		color = 0xFF000000,
-	}
-};
-
-Debug.Initialize = function()
-	Debug.FontObject = fonts.new(fontSettings2);	
-end
-Debug.Initialize();
---]]
-------------------------------------
+----------------------------------
 --- Check what menu we have open
 ------------------------------------
 local pGameMenu = ashita.memory.find('FFXiMain.dll', 0, "8B480C85C974??8B510885D274??3B05", 16, 0);
@@ -74,89 +59,104 @@ end
 ------------------------------------
 ashita.events.register('key', 'key_callback', function (e)
     local TargetItemID = AshitaCore:GetMemoryManager():GetInventory():GetSelectedItemId()
-        local base = "";
-        local stacksize = "";
-        local ahLocation = "";
+    local base = ""; 
+    local hxibase = ""; 
+    local stacksize = "";
+    local ahLocation = "";
+    
+    local outText = 'Price Check '..addon.version;
+    local baseText = "";
+    local stacksellText = "";
+    local ahLocationText ="";
+    local nametext = "";
+    local stackselltxt = "";
+    local database = "";
+    local mult = 1;
+    
         
-        local outText = 'Price Check';
-        local baseText = "";
-        local stacksellText = "";
-        local ahLocationText ="";
-        local priceMultiplyer 
     if TargetItemID ~= nil then
-       --AshitaCore:GetChatManager():AddChatMessage(8, false, tostring(TargetItemID))
-        --local base = "";
-        --local baseText = "";
-        --local outText = 'Price Check';
         local menuName = GetMenuName();
+        -- List of allowed menu keywords
         -- only display when these menus are open
         -- this prevents the text box from staying open when in non item related menus like magic or job ability 
-        local menuNameCheckAcu = string.find(menuName, "auc") 
-        local menuNameCheckMoneyctr = string.find(menuName, "moneyctr") 
-        local menuNameCheckInv = string.find(menuName, "inv") 
-        local menuNameCheckBank = string.find(menuName, "bank") 
-        local menuNameCheckEquip = string.find(menuName, "equip") 
-        local menuNameCheckLoot = string.find(menuName, "loot") 
-        local menuNameCheckDelivery = string.find(menuName, "delivery") 
-        local menuNameCheckShop = string.find(menuName, "shop") --
-
-        -- AshitaCore:GetChatManager():AddChatMessage(8, false, tostring(TargetItemID)) --chat message debug
+        local allowedMenus = {"auc", "moneyctr", "inv", "bank", "equip", "loot", "delivery", "shop"};
+        
+        -- Function to check if menu name contains any of the allowed keywords
+        local function isAllowedMenu(name)
+            for _, keyword in ipairs(allowedMenus) do
+                if string.find(name, keyword) then
+                    return true
+                end
+            end
+            return false
+        end
+    
         if (ItemIDTable[TargetItemID] ~= nil) then
-            --[[----Debugging text for debug text box
-            outTextDebug ="Price Check Debug"..'\n'..'menu='..menuName..'\n'..'ID='..TargetItemID;--debugging
-            Debug.FontObject.text = outTextDebug;--debugging
-            --]]
-            if ((menuNameCheckAcu~=nil) or (menuNameCheckMoneyctr~=nil) or (menuNameCheckInv~=nil) or (menuNameCheckBank~=nil) or (menuNameCheckEquip~=nil) or (menuNameCheckLoot~=nil) or (menuNameCheckDelivery~=nil) or (menuNameCheckShop~=nil))  then
+            if isAllowedMenu(menuName) then
                 base = ItemIDTable[TargetItemID].base;
                 name =  ItemIDTable[TargetItemID].name;
                 stacksize = ItemIDTable[TargetItemID].stacksize;
                 ahLocation = ItemIDTable[TargetItemID].ahLocation;
-                --check for nil text incase it isnt in the table
+           
+                hxibase = ItemIDTable[TargetItemID].hxibase;
+
+                mult = 1; -- by default mult is set to 1 and then modified 
+                database = "Lsb"
+
                 if base ~= nil then 
-                    if tonumber(base) == 0  then
-                        baseText= "BaseSell= 0";
+                    if (useHxiData == 1) and (hxibase ~= nil) then -- if useHxiData is set to 1 and hxibase is not nil then set base to the hxibase data
+                        base = hxibase;
+                        database = "Hxi"
+                    end
+
+                    if (base == 0) then -- Sinlge qty Sell range
+                        baseText= "Sell= 0";
                     else
-                        baseText = "BaseSell= ".. base.." ~ "..math.floor(base*1.225);
+                        if (base >= 160) then -- muilt factor changes based on the items basesell value
+                            mult = 1.025; -- rank 9 factor for all items greater than or equal to 160 base gil
+                        else
+                            mult = 1.10; -- items under 160 base gil
+                        end
+                        baseText = "SellRange= ".. base.." ~ "..math.floor(base*mult);
                     end
                     if stacksize ~= nil then
-                        if tonumber(stacksize) > 1 then
-                            if tonumber(base) == 0  then
-                                stackselltxt = ", \nStackSell("..stacksize..")= 0"
+                        if stacksize > 1 then
+                            if base == 0  then
+                                stackselltxt = "\nStackSell("..stacksize..")= 0"
                             else
-                                stackselltxt = ", \nStackSell("..stacksize..")= ".. base * stacksize .."~".. math.floor(base*1.225*stacksize) ;
+                                stackselltxt = "\nStackSell("..stacksize..")= ".. base * stacksize.." ~ ".. math.floor(base*mult)*stacksize ;
                             end
-                        else
-                            stackselltxt = "";
+
                         end
-                    else
-                        stackselltxt = ""
+
                     end
-                else
-                    baseText = "";
                 end
    
-                if name ~= nil then 
+                if name ~= nil then -- add item name to the print string
                     nametext = name;
-                else
-                    nametext = "";
                 end
 
-                if ahLocation~= nil then
+                if ahLocation~= nil then -- add auction house location to the string
                     ahLocationText = ahLocation;
                 end
-                outText = outText .. '\n'..nametext .." ["..TargetItemID..']\n'..baseText .. stackselltxt..'\n'..ahLocationText;
+                outText = outText .. '\n'..nametext .." ["..TargetItemID..'] |'..database..'|\n'..baseText .. stackselltxt..'\n'..ahLocationText;
                 PriceCheck.FontObject.text = outText;
             else
                 TargetItemID=""
                 outText="";
                 PriceCheck.FontObject.text = outText;
             end
+        else-- if the item is NOT in the ItemIDTable AND the TargetItemID is NOT nil then show this text
+            if isAllowedMenu(menuName) then
+            outText = outText.. "\nItem Not Found\nTargetItemID = "..TargetItemID;
+            PriceCheck.FontObject.text = outText;
+            end
         end
-        if TargetItemID == 0 then
+        
+        if (TargetItemID == 0) or(TargetItemID == nil) then
             TargetItemID=""
             outText="";
             PriceCheck.FontObject.text = outText;
         end--]]
-    --[[else;TargetItemID="";outText="";PriceCheck.FontObject.text = outText; --]]
     end
 end);
